399bd01434eb8bd8c7757a6893e0f80b7215b5ee
Modson Tande <131668620+Modsontech45@users.noreply.github.com>
2025-08-08 12:11:34 +0000

    Update site

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Detect if running in GitHub Pages
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-    base: '/synctuario/', //
+  base: isGitHubPages ? '/synctuario/' : '/', // dynamic base
 });
